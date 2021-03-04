34babf893b4daef2cf86732c5d098cec1cd48860
Wing Lam <Wing.Lam@mssl.co.uk>
2021-03-04 15:39:27 +0000

    tidied up the code and styled the default, generic, error page.

--- PWA_Test/Controllers/CaseController.cs
+++ PWA_Test/Controllers/CaseController.cs
@@ -16,8 +16,22 @@ namespace PWA_Test.Controllers
             request.AddHeader("Content-Type", "application/xml");
 
             // This will be coming from selection from a list 
-            request.AddParameter("application/xml", "<sctlink-request>\r\n    <login>\r\n        <email>sdlcomps2020@rightmove.com</email>\r\n        <password>EWv=2!!WY!Dn6H</password>\r\n        <officeid>215927</officeid>\r\n    </login>\r\n    <address>\r\n        <address-line-1>15 Berrans Avenue</address-line-1>\r\n        <address-line-2>Kinson</address-line-2>\r\n        <address-line-3>Bournemouth</address-line-3>\r\n        <postcode>BH11 9BT</postcode>\r\n    </address>\r\n    <quest-xit2-reference>1234</quest-xit2-reference>\r\n</sctlink-request>",
+            request.AddParameter("application/xml", 
+                                  "<sctlink-request>\r\n    " +
+                                    "<login>\r\n        " +
+                                    "  <email>sdlcomps2020@rightmove.com</email>\r\n        " +
+                                    "  <password>EWv=2!!WY!Dn6H</password>\r\n        " +
+                                    "  <officeid>215927</officeid>\r\n    " +
+                                    "</login>\r\n    " +
+                                    "<address>\r\n        " +
+                                    "<address-line-1>15 Berrans Avenue</address-line-1>\r\n        " +
+                                    "<address-line-2>Kinson</address-line-2>\r\n        " +
+                                    "<address-line-3>Bournemouth</address-line-3>\r\n        " +
+                                    "<postcode>BH11 9BT</postcode>\r\n    </address>\r\n    " +
+                                    "<quest-xit2-reference>1234</quest-xit2-reference>\r\n" +
+                                  "</sctlink-request>",
                     ParameterType.RequestBody);
+
             IRestResponse response = client.Execute(request);
 
             //Checking the response is successful or not which is sent using HttpClient
@@ -30,8 +44,6 @@ namespace PWA_Test.Controllers
 
                 var model = new Case
                 {
-                    officeId = xml.Element("officeid").Value,
-                    rmReference = xml.Element("rm-reference").Value,
                     link = xml.Element("link").Value,
                 };
 
