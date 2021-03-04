a31117a3312b2b0fad642bc80ffa3d74f74b9b84
Wing Lam <Wing.Lam@mssl.co.uk>
2021-03-04 12:15:20 +0000

    Added the image tag to show the image on the comparable details page.
    Did more research work for error handling.

--- PWA_Test/App_Start/FilterConfig.cs
+++ PWA_Test/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace PWA_Test
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
